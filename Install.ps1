0916579aca94c1ad74b25272d6a461e265c3f41a
Vincent Yiu <vincent.yiu@internal.mdsec.co.uk>
2017-07-17 14:44:47 +0100

    Install script for Windows

---
+++ Install.ps1
@@ -0,0 +1,7 @@
+﻿$path = Resolve-Path .\json\json.jar | Select-String json
+$path = $path -replace "\\","/"
+$path = $path -replace "C:",""
+write-output $path
+
+$angrypuppy = Get-Content .\ANGRYPUPPY.cna
+$angrypuppy -replace "{{PATH}}",$path | set-content .\ANGRYPUPPY.cna
